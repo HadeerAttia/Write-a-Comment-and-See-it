4d8376e55654e32376f47a13bd4061173b79b054
HadeerAttia <hadeer_attia@yahoo.com>
2015-05-16 10:21:30 +0200

    Create writeCommentJS.js

---
+++ writeCommentJS.js
@@ -0,0 +1,12 @@
+//leaveComment is a Class add to the Write a Comment Button 
+//inputPlace is a Class add to input text 
+$(".leaveComment").each(function () {
+        $(this).click(function () {
+            var hisComment = $(this).prev('.inputPlace').val();
+            var lineHr = $("<hr/>");
+            var divComment = $("<div>" + hisComment + "</div>");
+            $(this).parent().parent().parent().parent().next().children().children().append(lineHr);
+            $(this).parent().parent().parent().parent().next().children().children().append(divComment);
+            $(this).prev('.inputPlace').val("");
+        });
+    });
